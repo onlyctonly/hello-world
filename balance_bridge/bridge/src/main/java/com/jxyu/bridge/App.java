d09c15336879f7f554138592213b845df65339e1
XiangYu <onlyctonly@gmail.com>
2018-03-24 16:48:51 +0200

    label readygit statusgit statusgit statusgit statusgit status

--- balance_bridge/bridge/src/main/java/com/jxyu/bridge/App.java
+++ balance_bridge/bridge/src/main/java/com/jxyu/bridge/App.java
@@ -1,25 +1,67 @@
 package com.jxyu.bridge;
 
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.util.EntityUtils;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-    		CloseableHttpClient httpClient = HttpClients.createDefault();
-    		HttpGet httpGet = new HttpGet("http://www.google.com/");
+    	
+    	//system property
+    	//java -Dbridge.restart.type=full
+    	System.out.println(System.getProperty("bridge.restart.type"));
+    	
+    	//logging
+    	final Logger logger= Logger.getLogger(App.class.getName());
+    	//create filehandler and set to append mode
+    	FileHandler fileTxt = new FileHandler("Logging.txt", true);
+    	
+    	SimpleFormatter sf = new SimpleFormatter();
+    	fileTxt.setFormatter(sf);
+    	logger.addHandler(fileTxt);
+    	String t = System.getProperty("bridge.restart.type");
+    	if (t!=null) {
+        	logger.info(t);
+		}
+    	//set cookie
+    	CookieStore cookieStore = new BasicCookieStore();
+    	BasicClientCookie cookie = new BasicClientCookie("name", "value");
+    	cookie.setDomain(".goolge.com");
+    	cookie.setPath("/");
+    	cookieStore.addCookie(cookie);
+
+    	
+    		// attach cookie
+    		CloseableHttpClient httpClient = HttpClients.custom()
+    				 .setDefaultCookieStore(cookieStore)
+    				 .build();
+    		HttpGet httpGet = new HttpGet("http://www.baidu.com");
     		System.out.println("executing get request: " + httpGet.getURI());
-    		
+    		logger.info(httpGet.getURI().toString());
+			CloseableHttpResponse res = null;
+			try {
+				res = httpClient.execute(httpGet);
+			} catch (ClientProtocolException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
     		try {
-				CloseableHttpResponse res = httpClient.execute(httpGet);
 				HttpEntity entity = res.getEntity();
 				System.out.println(res.getStatusLine());
 				
@@ -27,12 +69,13 @@ public class App
 					System.out.println(entity.getContentLength());
 					System.out.println(EntityUtils.toString(entity));
 				}
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
+    		}
+			 catch (ClientProtocolException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} finally {
+				res.close();
 			}
     		
     		
--- pdf/pdf/src/main/java/gr/cgw/word/Main.java
+++ pdf/pdf/src/main/java/gr/cgw/word/Main.java
@@ -1,33 +1,85 @@
 package gr.cgw.word;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.hwpf.HWPFDocument;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-	      //Blank Document
-	      XWPFDocument document = new XWPFDocument(); 
-	      
-	      //Write the Document in file system
-	      FileOutputStream out = new FileOutputStream(new File("createparagraph.docx"));
-	        
-	      //create Paragraph
-	      XWPFParagraph paragraph = document.createParagraph();
-	      XWPFRun run = paragraph.createRun();
-	      run.setText("At tutorialspoint.com, we strive hard to " +
-	         "provide quality tutorials for self-learning " +
-	         "purpose in the domains of Academics, Information " +
-	         "Technology, Management and Computer Programming Languages.");
-				
-	      document.write(out);
-	      out.close();
-	      System.out.println("createparagraph.docx written successfully");
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/subscription?useSSL=false";
+	static final String USER = "root";
+	static final String PASS = "root";
 
+	public static void main(String[] args) throws Exception {
+		// mysql
+		Connection conn = null;
+		Statement stmt = null;
+		// Class.forName("com.mysql.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+		stmt = conn.createStatement();
+		String sql;
+		sql = "SELECT title, givenname, surname,department,street,street_number,area,tk FROM greeksubscriber";
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		File file = new File("template.doc");
+		InputStream fs = new FileInputStream(file);
+		HWPFDocument doc2 = new HWPFDocument(fs);
+		
+		while (rs.next()) {
+			String title = rs.getString("title");
+			String surname = rs.getString("surname");
+			String givenname = rs.getString("givenname");
+			String department = rs.getString("department");
+			String street = rs.getString("street");
+			String no = rs.getString("street_number");
+			String area = rs.getString("area");
+			String tk = rs.getString("tk");
+			String total = title + " " + givenname + " " + surname + "\n" + department + ", " + street + " " + no + ", "
+					+ area + ", " + tk;
+			doc2.getRange().replaceText("${1}", total);
+		}
+		rs.close();
+		stmt.close();
+		conn.close();
+		System.out.println("Goodbye!");
+
+		// open a file
+
+		doc2.write(new File("2.doc"));
+		doc2.close();
 	}
 
 }
+
+// Blank Document
+// XWPFDocument document = new XWPFDocument();
+//
+//// Write the Document in file system
+// FileOutputStream out = new FileOutputStream(new
+// File("createparagraph.docx"));
+//
+//// create table
+// XWPFTable table = document.createTable();
+// XWPFTableRow tableRowOne = table.getRow(0);
+// tableRowOne.setHeight(17000);
+// tableRowOne.getCell(0).setText("col one, row one");
+// tableRowOne.addNewTableCell().setText("col two, row one");
+// tableRowOne.addNewTableCell().setText("col three, row one,bla,bla,bal");
+//
+//// create Paragraph
+// XWPFParagraph paragraph = document.createParagraph();
+// XWPFRun run = paragraph.createRun();
+// run.setText("测试");
+//
+// document.write(out);
+// out.close();
+// document.close();
+// System.out.println("createparagraph.docx written successfully");
