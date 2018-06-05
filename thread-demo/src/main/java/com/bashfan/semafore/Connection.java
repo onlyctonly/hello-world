4de1be6a74e352a247f1544d47ff0a5264cbcb32
XiangYu Jia <onlyctonly@gmail.com>
2018-06-05 21:27:52 +0300

    thread

--- thread-demo/src/main/java/com/bashfan/semafore/Connection.java
+++ thread-demo/src/main/java/com/bashfan/semafore/Connection.java
@@ -1,4 +1,46 @@
 package com.bashfan.semafore;
 
+import java.util.concurrent.Semaphore;
+
 public class Connection {
+    private static Connection instance = new Connection();
+    private Semaphore semaphore = new Semaphore(10,true);
+    private int connections = 0;
+
+    private Connection() {
+    }
+
+    public static Connection getInstance() {
+        return instance;
+    }
+
+    public void connect() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            doConnect();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            semaphore.release();
+        }
+    }
+
+    public void doConnect() throws InterruptedException {
+        synchronized (this) {
+            connections++;
+            System.out.println(connections);
+        }
+
+        Thread.sleep(2000);
+
+        synchronized (this) {
+            connections--;
+            System.out.println(connections);
+        }
+    }
 }
